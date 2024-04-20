8451565782401cf8f88f4055df7c693a497c81ba
abhinz <abhinandorg3@gmail.com>
2024-04-20 07:49:50 +0530

    video tag width

--- src/Pages/Teacher/Course/TeacherVideoPlayer.js
+++ src/Pages/Teacher/Course/TeacherVideoPlayer.js
@@ -390,7 +390,7 @@ console.log('videoUrl',videoUrl)
                     {videoUrl && (
                     <div className="lg:col-span-4 w-200">
                         <div>
-                        <video className="" autoplay controls >
+                        <video style={{width:700}} className="" autoplay controls >
                         <source src={baseURL+videoUrl} type="video/mp4" />
                         Your browser does not support the video tag.
                         </video>
